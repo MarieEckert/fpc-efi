88c7d289e6cd53c483c70a4834b4c5e09f2dd4a0
Marie <marie@ha.si>
2026-09-09 03:19:46 +0200

    expand efilib

--- src/efilib.pas
+++ src/efilib.pas
@@ -1,6 +1,10 @@
 {$mode fpc}
 unit efilib;
 
+{$scopedenums on}
+{$writeableconst off}
+{$typedaddress on}
+
 { Bindings for UEFI data structures and functions.
   Built with the UEFI Specifcation Version 2.6.
 }
@@ -58,7 +62,19 @@ procedure Print(fmt: PChar16);
 {$endif}
 
 type
-	TEfiLibError		= (eleOK, eleNoSystemTable, eleEfiCallFailure);
+	TEfiLibMemoryMap = record
+		Size				: TUINTN;
+		Map					: PEfiMemoryDescriptor;
+		Key					: TUINTN;
+		DescriptorSize		: TUINTN;
+		DescriptorVersion	: UInt32;
+	end;
+
+	{ Result / Return Types }
+	TEfiLibError		= (OK,
+						   NoSystemTable,
+						   EfiCallFailure,
+						   NilValue);
 	TEfiLibErrorDetail	= UInt64;
 
 	TMemoryPoolResult = record
@@ -67,15 +83,79 @@ type
 		Pool	: TMemoryPool;
 	end;
 
+	TVolumeResult = record
+		Status	: TEfiLibError;
+		Detail	: TEfiLibErrorDetail;
+		Point	: UInt64; { debug! remove? }
+		Volume	: PEfiFileProtocol;
+	end;
+
+	TFileInfoResult = record
+		Status		: TEfiLibError;
+		Detail		: TEfiLibErrorDetail;
+		FileInfo	: PEfiFileInfo;
+	end;
+
+	TLoadFileResult = record
+		Status		: TEfiLibError;
+		Detail		: TEfiLibErrorDetail;
+		Msg			: PWideChar;
+
+		Handle		: PEfiFileHandle;
+		Buffer		: TMemoryPool;
+	end;
+
+	TMemoryMapResult = record
+		Status		: TEfiLibError;
+		Detail		: TEfiLibErrorDetail;
+		MemoryMap	: TEfiLibMemoryMap;
+	end;
+
+	TGOPResult = record
+		Status		: TEfiLibError;
+		Detail		: TEfiLibErrorDetail;
+		GOP			: PEfiGraphicsOutputProtocol;
+	end;
+
+{ Printing Functions }
+
 procedure Print(constref msg: PWideChar);
 
 procedure PrintErrorStr(err: TEfiLibError);
 
 procedure PrintHex(_int: UInt64);
 
-procedure PrintError(constref msg: PWideChar; err: TEfiLibError; detail: UInt64);
+procedure PrintError(
+	constref msg: PWideChar;
+	err: TEfiLibError; detail: UInt64
+);
 
-function AllocateMemoryPool(const PoolSize: TUINTN): TMemoryPoolResult;
+{ Memory }
+
+function AllocPool(const PoolSize: TUINTN): TMemoryPoolResult;
+
+procedure FreePool(var Pool: TMemoryPool);
+
+function GetMemoryMap: TMemoryMapResult;
+
+{ File I/O Utilities }
+
+function GetVolume(Image: TEfiHandle): TVolumeResult;
+
+function GetFileInfo(
+	FHnd: PEfiFileHandle
+): TFileInfoResult;
+
+function LoadFile(
+	const Path: PWideChar;
+	const Volume: PEfiFileProtocol
+): TLoadFileResult;
+
+{ function FileInfoGetName(const Info: PEfiFileInfo): PChar16; }
+
+{ GOP }
+
+function GetGOP: TGOPResult;
 
 var
 	SystemTable	: PEfiSystemTable	= Nil;
@@ -91,9 +171,10 @@ end;
 procedure PrintErrorStr(err: TEfiLibError);
 begin
 	case err of
-	eleOK: Print('eleOK');
-	eleNoSystemTable: Print('eleNoSystemTable');
-	eleEfiCallFailure: Print('eleEfiCallFailure');
+	TEfiLibError.OK: Print('OK');
+	TEfiLibError.NoSystemTable: Print('NoSystemTable');
+	TEfiLibError.EfiCallFailure: Print('EfiCallFailure');
+	TEfiLibError.NilValue: Print('NilValue');
 	else Print('unknownError');
 	end;
 end;
@@ -101,11 +182,10 @@ end;
 procedure PrintHex(_int: UInt64);
 const
 	MAX_DIGITS = 32;
-	DIGITS: array of WideChar = ('0','1','2','3','4','5','6','7','8','9','A','B','C',
-								 'D','E','F');
+	DIGITS: array of WideChar = ('0','1','2','3','4','5','6','7',
+								 '8','9','A','B','C','D','E','F');
 var
 	wix, ix: Integer;
-	b: UInt8;
 	c: array [0..1] of WideChar;
 	_str: array [0..MAX_DIGITS] of WideChar;
 begin
@@ -117,6 +197,8 @@ begin
 	wix := High(_str);
 	_str[wix] := WideChar($0000);
 	Dec(wix);
+
+	for ix := wix downto 0 do _str[ix] := '0';
 
 	ix := 0;
 
@@ -130,7 +212,10 @@ begin
 	Print(PWideChar(_str) + wix);
 end;
 
-procedure PrintError(constref msg: PWideChar; err: TEfiLibError; detail: UInt64);
+procedure PrintError(
+	constref msg: PWideChar;
+	err: TEfiLibError; detail: UInt64
+);
 begin
 	Print(msg);
 	PrintErrorStr(err);
@@ -139,32 +224,284 @@ begin
 	Print(''#13#10);
 end;
 
-function AllocateMemoryPool(const PoolSize: TUINTN): TMemoryPoolResult;
+function AllocPool(const PoolSize: TUINTN): TMemoryPoolResult;
 var
 	pool: TMemoryPool;
 begin
 	if SystemTable = Nil then
 	begin
-		AllocateMemoryPool.Status := eleNoSystemTable;
+		AllocPool.Status := TEfiLibError.NoSystemTable;
 		exit;
 	end;
 
-	AllocateMemoryPool.Detail :=
+	AllocPool.Detail :=
 		SystemTable^.BootServices^.AllocatePool(
-			EfiLoaderData,
-			PoolSize,
+			TEfiMemoryType.EfiLoaderData,
+			PoolSize + 4096,
 			@pool.Buffer
 		);
 
-	if AllocateMemoryPool.Detail <> EFI_SUCCESS then
+	if AllocPool.Detail <> EFI_SUCCESS then
 	begin
-		AllocateMemoryPool.Status := eleEfiCallFailure;
+		AllocPool.Status := TEfiLibError.EfiCallFailure;
 		exit;
 	end;
 
+	pool.Buffer += 4096 - (UInt64(pool.Buffer) mod 4096);
+
 	pool.Size := PoolSize;
-	AllocateMemoryPool.Pool := pool;
-	AllocateMemoryPool.Status := eleOK;
+	AllocPool.Pool := pool;
+	AllocPool.Status := TEfiLibError.OK;
+end;
+
+procedure FreePool(var Pool: TMemoryPool);
+begin
+	if Pool.Buffer = Nil then exit;
+	SystemTable^.BootServices^.FreePool(Pool.Buffer);
+end;
+
+function GetMemoryMap: TMemoryMapResult;
+var
+	memoryMap	: TEfiLibMemoryMap = (
+		Size: 0; Map: Nil; Key: 0; DescriptorSize: 0; DescriptorVersion: 0
+	);
+	allocResult	: TMemoryPoolResult;
+begin
+	GetMemoryMap.Detail := SystemTable^.BootServices^.GetMemoryMap(
+		@memoryMap.Size,
+		 memoryMap.Map,
+		@memoryMap.Key,
+		@memoryMap.DescriptorSize,
+		@memoryMap.DescriptorVersion
+	);
+
+	if GetMemoryMap.Detail <> EFI_BUFFER_TOO_SMALL then
+	begin
+		GetMemoryMap.Status := TEfiLibError.EfiCallFailure;
+		exit;
+	end;
+
+	memoryMap.Size += memoryMap.DescriptorSize * 2;
+	allocResult := AllocPool(memoryMap.Size);
+
+	if allocResult.Status <> TEfiLibError.OK then
+	begin
+		GetMemoryMap.Status := allocResult.Status;
+		GetMemoryMap.Detail := allocResult.Detail;
+		exit;
+	end;
+
+	memoryMap.Map := allocResult.Pool.Buffer;
+
+	GetMemoryMap.Detail := SystemTable^.BootServices^.GetMemoryMap(
+		@memoryMap.Size,
+		 memoryMap.Map,
+		@memoryMap.Key,
+		@memoryMap.DescriptorSize,
+		@memoryMap.DescriptorVersion
+	);
+
+	if GetMemoryMap.Detail <> EFI_SUCCESS then
+	begin
+		GetMemoryMap.Status := TEfiLibError.EfiCallFailure;
+		FreePool(allocResult.Pool);
+		exit;
+	end;
+
+	GetMemoryMap.Status := TEfiLibError.OK;
+	GetMemoryMap.MemoryMap := memoryMap;
+end;
+
+function GetVolume(image: TEfiHandle): TVolumeResult;
+var
+	loadedImage	: PEfiLoadedImageProtocol;
+	lipGuid		: TEfiGuid;
+	ioVolume	: PEfiSimpleFileSystemProtocol;
+	fsGuid		: TEfiGuid;
+	volume		: PEfiFileProtocol;
+begin
+	loadedImage := Nil;
+	lipGuid := EFI_LOADED_IMAGE_PROTOCOL_GUID;
+	fsGuid := EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+
+	GetVolume.Detail := SystemTable^.BootServices^.LocateProtocol(
+		PEfiGuid(@lipGuid[0]),
+		Nil,
+		PPointer(@loadedImage)
+	);
+
+	if GetVolume.Detail <> EFI_SUCCESS then
+	begin
+		GetVolume.Status := TEfiLibError.EfiCallFailure;
+		GetVolume.Point := 0;
+		exit;
+	end;
+
+	GetVolume.Detail := SystemTable^.BootServices^.HandleProtocol(
+		image,
+		PEfiGuid(@lipGuid[0]),
+		PPointer(@loadedImage)
+	);
+
+	if GetVolume.Detail <> EFI_SUCCESS then
+	begin
+		GetVolume.Status := TEfiLibError.EfiCallFailure;
+		GetVolume.Point := 1;
+		exit;
+	end;
+
+	GetVolume.Detail := SystemTable^.BootServices^.HandleProtocol(
+		loadedImage^.DeviceHandle,
+		PefiGuid(@fsGuid[0]),
+		PPointer(@ioVolume)
+	);
+
+	if GetVolume.Detail <> EFI_SUCCESS then
+	begin
+		GetVolume.Status := TEfiLibError.EfiCallFailure;
+		GetVolume.Point := 2;
+		exit;
+	end;
+
+	GetVolume.Detail := ioVolume^.OpenVolume(
+		ioVolume,
+		@volume
+	);
+
+	if GetVolume.Detail <> EFI_SUCCESS then
+	begin
+		GetVolume.Status := TEfiLibError.EfiCallFailure;
+		GetVolume.Point := 3;
+		exit;
+	end;
+
+	GetVolume.Status := TEfiLibError.OK;
+	GetVolume.Volume := volume;
+end;
+
+function GetFileInfo(
+			FHnd: PEfiFileHandle
+		): TFileInfoResult;
+var
+	poolRes	: TMemoryPoolResult;
+	status	: TEfiStatus;
+begin
+	poolRes := AllocPool(sizeof(TEfiFileInfo) + 128);
+	if poolRes.Status <> TEfiLibError.OK then
+	begin
+		GetFileInfo.Status := poolRes.Status;
+		GetFileInfo.Detail := poolRes.Detail;
+		exit;
+	end;
+
+	GetFileInfo.Status := TEfiLibError.OK;
+	GetFileInfo.FileInfo := poolRes.Pool.Buffer;
+
+	while True do
+	begin
+		status := FHnd^.GetInfo(
+			FHnd,
+			@EFI_FILE_INFO_GUID,
+			@poolRes.Pool.Size,
+			GetFileInfo.FileInfo
+		);
+
+		if status = EFI_SUCCESS then
+			break;
+
+		if status <> EFI_BUFFER_TOO_SMALL then
+		begin
+			FreePool(poolRes.pool);
+			GetFileInfo.Status := TEfiLibError.EfiCallFailure;
+			GetFileInfo.Detail := status;
+			exit;
+		end;
+
+		FreePool(poolRes.pool);
+
+		poolRes := AllocPool(poolRes.Pool.Size + 64);
+		if poolRes.Status <> TEfiLibError.OK then
+		begin
+			GetFileInfo.Status := poolRes.Status;
+			GetFileInfo.Detail := poolRes.Detail;
+			exit;
+		end;
+	end;
+end;
+
+function LoadFile(
+	const Path: PWideChar;
+	const Volume: PEfiFileProtocol
+): TLoadFileResult;
+var
+	finfResult	: TFileInfoResult;
+	allocResult	: TMemoryPoolResult;
+begin
+	LoadFile.Detail := Volume^.Open(
+		Volume,
+		@LoadFile.Handle,
+		Path,
+		EFI_FILE_MODE_READ,
+		EFI_FILE_READ_ONLY or EFI_FILE_HIDDEN or EFI_FILE_SYSTEM
+	);
+	if LoadFile.Detail <> EFI_SUCCESS then
+	begin
+		LoadFile.Status := TEfiLibError.EfiCallFailure;
+		LoadFile.Msg := 'Volume^.Open failed: ';
+		exit;
+	end;
+
+	finfResult := efilib.GetFileInfo(LoadFile.Handle);
+	if finfResult.Status <> TEfiLibError.Ok then
+	begin
+		LoadFile.Status := finfResult.Status;
+		LoadFile.Detail := finfResult.Detail;
+		LoadFile.Msg := 'GetFileInfo failed: ';
+		LoadFile.Handle^.Close(LoadFile.Handle);
+		exit;
+	end;
+
+	allocResult := efilib.AllocPool(finfResult.FileInfo^.FileSize);
+	if allocResult.Status <> TEfiLibError.Ok then
+	begin
+		LoadFile.Status := allocResult.Status;
+		LoadFile.Detail := allocResult.Detail;
+		LoadFile.Msg := 'AllocPool failed: ';
+		LoadFile.Handle^.Close(LoadFile.Handle);
+	end;
+
+	LoadFile.Buffer := allocResult.Pool;
+
+	LoadFile.Detail := LoadFile.Handle^.Read(
+		LoadFile.Handle,
+		@LoadFile.Buffer.Size,
+		LoadFile.Buffer.Buffer
+	);
+	if LoadFile.Detail <> EFI_SUCCESS then
+	begin
+		LoadFile.Status := TEfiLibError.EfiCallFailure;
+		LoadFile.Msg := 'Handle^.Read failed: ';
+		LoadFile.Handle^.Close(LoadFile.Handle);
+	end;
+
+	LoadFile.Status := TEfiLibError.Ok;
+	LoadFile.Msg := 'Ok';
+end;
+
+function GetGOP: TGOPResult;
+begin
+	GetGOP.Status := TEfiLibError.OK;
+	GetGOP.Detail := SystemTable^.BootServices^.LocateProtocol(
+		@EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+		Nil,
+		PPointer(@GetGOP.GOP)
+	);
+
+	if GetGOP.Detail <> EFI_SUCCESS then
+	begin
+		GetGOP.Status := TEfiLibError.EfiCallFailure;
+		exit;
+	end;
 end;
 
 end.
